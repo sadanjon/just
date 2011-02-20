6191389c3dd0b37e7b36e556429501fb0b0d63b8
Jonathan Sadan <sadanjon@gmail.com>
2011-02-20 02:27:49 +0200

    Added GPL license.

--- just.py
+++ just.py
@@ -1,5 +1,22 @@
 #! /usr/bin/python
 
+#   Copyright 2011 Jonathan Sadan
+#
+#   This file is part of 'just'.
+#
+#   'just' is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   'just' is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with 'just'.  If not, see <http://www.gnu.org/licenses/>.
+
 import argparse, os, sys, graphtools
 
 def create_args_parser():
